fb8360361264693b8fde2dc1bbdcf5354d970cc4
Chaitanya Datar <datar.chaitanya@gmail.com>
2025-09-13 22:30:17 +0530

    Create solution.cs

---
+++ solution.cs
@@ -0,0 +1,70 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode c1 = l1;
+        ListNode c2 = l2;
+        ListNode prev = new ListNode(0, c1);
+        int carry = 0;
+
+        while(c1 != null && c2 != null){
+            c1. val = c1.val + c2.val + carry;
+            carry = 0;
+            if(c1.val >= 10){
+                c1.val = c1.val % 10;
+                carry = 1;
+            }
+            c1 = c1.next;
+            c2 = c2.next;
+            prev = prev.next;
+        }        
+
+        if(c1 == null){
+            prev.next = c2;
+            c1 = c2;
+            while(carry == 1 && c1 != null){
+                c1.val = c1.val + carry;
+                carry = 0;
+                if(c1.val >= 10){
+                    c1.val = c1.val % 10;
+                    carry = 1;
+                }
+                c1 = c1.next;
+                prev = prev.next;
+            }
+
+            if(c1 == null && carry == 1){
+                ListNode t = new ListNode(1, null);
+                prev.next = t;
+            }
+        }
+
+        if(c2 == null){
+            while(carry == 1 && c1 != null){
+                c1.val = c1.val + carry;
+                carry = 0;
+                if(c1.val >= 10){
+                    c1.val = c1.val % 10;
+                    carry = 1;
+                }
+                c1 = c1.next;
+                prev = prev.next;
+            }
+
+            if(c1 == null && carry == 1){
+                ListNode t = new ListNode(1, null);
+                prev.next = t;
+            }
+        }
+        return(l1);
+    }
+}
